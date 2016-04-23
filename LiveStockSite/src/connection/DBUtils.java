47ee47c470a49791b48730e29e0ec1386ba812b3
Yuliya Astapova <ellemandora@gmail.com>
2016-04-22 22:41:09 -0400

    finished Managers and employees

--- LiveStockSite/src/connection/DBUtils.java
+++ LiveStockSite/src/connection/DBUtils.java
@@ -101,27 +101,7 @@ public class DBUtils {
 		return null;
 	}
 
-	public static UserAccount findUser(Connection conn, String userName) throws SQLException {
-
-		String sql = "Select a.User_Name, a.Password, a.Gender from User_Account a " + " where a.User_Name = ? ";
-
-		PreparedStatement pstm = conn.prepareStatement(sql);
-		pstm.setString(1, userName);
-
-		ResultSet rs = pstm.executeQuery();
-
-		if (rs.next()) {
-			String password = rs.getString("Password");
-			String gender = rs.getString("Gender");
-			UserAccount user = new UserAccount();
-			user.setUserName(userName);
-			user.setPassword(password);
-			user.setGender(gender);
-			return user;
-		}
-		return null;
-	}
-
+	
 /*
 	public static List<Product> queryProduct(Connection conn) throws SQLException {
 		String sql = "Select a.Code, a.Name, a.Price from Product a ";
